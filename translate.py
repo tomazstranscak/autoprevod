4c0f8befdb96be6517825f13e7b312eb93924d3b
prep <prep@localhost.localdomain>
2024-10-04 08:34:56 +0200

    dela?

--- translate.py
+++ translate.py
@@ -2,14 +2,13 @@ from googletrans import Translator
 import os
 
 def translate_file(fajl, target_language):
+
     translator = Translator()
     
     with open(fajl, 'r', encoding='utf-8') as file:
         content = file.read()
 
     translated = translator.translate(content, dest=target_language)
-    
-    
     output_file = os.path.join(os.getcwd(), f'readme_{target_language}.md')
     with open(output_file, 'w', encoding='utf-8') as file:
         file.write(translated.text)
@@ -24,6 +23,8 @@ if __name__ == "__main__":
     lang_file = 'jeziki.md'
 
     languages = read_languages(lang_file)
-
     for lang in languages:
-        translate_file(fajl, lang)
+        try:
+            translate_file(fajl, lang)
+        except:
+            pass
